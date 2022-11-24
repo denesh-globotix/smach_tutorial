325370c05b18fae1683a0d59cd29bfb9f8169f28
Denesh <denesh@u.nus.edu>
2022-11-24 09:26:36 +0800

    Adding the ability to enter the alternate outcome

--- test_smach.py
+++ test_smach.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import rospy
 import smach
-from smach import State
+from random import randrange
 
-# define state Foo
+# define state Code
 class Code(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['time_to_sleep','done'])
+        smach.State.__init__(self, outcomes=['time_to_sleep','done', 'burned'])
         self.counter = 0
 
     def execute(self, userdata):
@@ -16,10 +16,12 @@ class Code(smach.State):
             rospy.loginfo("Time to sleep")
             return 'time_to_sleep'
         else:
-            return 'done'
+            if(randrange(2)):
+                return 'done'
+            else:
+                return 'burned'
 
-
-# define state Bar
+# define state Sleep
 class Sleep(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['continue_coding'])
@@ -27,9 +29,6 @@ class Sleep(smach.State):
     def execute(self, userdata):
         rospy.loginfo('Done sleeping get back to coding')
         return 'continue_coding'
-        
-
-
 
 def initialiseStateMachine():
     # Initialise the ros node
@@ -43,7 +42,8 @@ def initialiseStateMachine():
         # Add states to the container
         smach.StateMachine.add('CODE', Code(), 
                                transitions={'time_to_sleep':'SLEEP', 
-                                            'done':'project_delivered'})
+                                            'done':'project_delivered',
+                                            'burned': 'burned_out'})
         smach.StateMachine.add('SLEEP', Sleep(), 
                                transitions={'continue_coding':'CODE'})
 
